9a0f11f20211fb2b77b2c1c1ef030cd71e6bb813
ChrisH <geochris35@gmail.com>
2018-03-10 23:48:23 -0600

    updated SECRET_KEY to env variable

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1%ipmpy@&=s(glmzyl!fnq55xn1qms!t9ereuo2&c8e9wifc+#'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
