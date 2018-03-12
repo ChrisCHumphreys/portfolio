d39943c34c44083c233246ed6d4e8d30e7b1ea1a
ChrisH <geochris35@gmail.com>
2018-03-11 20:43:43 -0500

    updated production.py settings for staticfiles

--- portfolio_project/production.py
+++ portfolio_project/production.py
@@ -126,8 +126,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 BOOTSTRAP4 = { 'theme_url':
                '/static/bootstrap/css/bootstrap.min.css',
