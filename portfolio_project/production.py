46a944def32221f386af620b725f10eac7bde49d
ChrisH <geochris35@gmail.com>
2018-03-11 20:45:50 -0500

    production settings were working.  updated allowed hosts and set debug = false

--- portfolio_project/production.py
+++ portfolio_project/production.py
@@ -23,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['chrishumphreyspf.herokuapp.com']
 
 # Application definition
 
